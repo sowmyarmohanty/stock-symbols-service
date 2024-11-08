ffccbdfe89a3c80144e511daf81e4d0feb373317
Sowmya Mohanty <83002502+sowmyarmohanty@users.noreply.github.com>
2024-11-08 09:38:32 +0530

    initial checkin

---
+++ src/main/java/com/portfolioai/service/stocksymbols/StocksymbolsApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioai.service.stocksymbols;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksymbolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksymbolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolioai/service/stocksymbols/StocksymbolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioai.service.stocksymbols;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksymbolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
